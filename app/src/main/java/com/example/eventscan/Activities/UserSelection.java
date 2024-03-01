872dd309c51a5386dc88728c2293143fc8d0ece6
Bern Kairo Aran <bern@ualberta.ca>
2024-02-29 19:55:15 -0700

    default screen
    created and changed the default activity and screen

--- app/src/main/java/com/example/eventscan/Activities/UserSelection.java
+++ app/src/main/java/com/example/eventscan/Activities/UserSelection.java
@@ -1,8 +1,33 @@
 package com.example.eventscan.Activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.eventscan.Entities.Administrator;
+import com.example.eventscan.Entities.Attendee;
+import com.example.eventscan.Entities.Organizer;
+import com.example.eventscan.Entities.User;
+import com.example.eventscan.R;
+
 public class UserSelection extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userselection);
+
+        // Handle user type selection
+        Button userType1Button = findViewById(R.id.buttonOrganizer);
+        Button userType2Button = findViewById(R.id.buttonAttendee);
+        Button userType3Button = findViewById(R.id.buttonAdministrator);
+
+        //userType1Button.setOnClickListener(view -> startApp(Organizer.class));
+        //userType2Button.setOnClickListener(view -> startApp(Attendee.class));
+        //userType3Button.setOnClickListener(view -> startApp(Administrator.class));
+    }
+
 }
+
---
+++ app/src/main/java/com/example/eventscan/Entities/Administrator.java
@@ -0,0 +1,7 @@
+package com.example.eventscan.Entities;
+
+public class Administrator extends User{
+    public Administrator(String name, String password) {
+        super(name, password);
+    }
+}
