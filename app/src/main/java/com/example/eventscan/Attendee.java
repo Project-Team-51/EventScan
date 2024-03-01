0078a785d7664f8a9e8ccdb9435953774ee0d290
Bern Kairo Aran <bern@ualberta.ca>
2024-02-29 18:59:56 -0700

    adding folders to put specific java files into

--- app/src/main/java/com/example/eventscan/Attendee.java
+++
@@ -1,7 +0,0 @@
-package com.example.eventscan;
-
-public class Attendee extends User{
-    public Attendee(String name, String password) {
-        super(name, password);
-    }
-}
---
+++ app/src/main/java/com/example/eventscan/Entities/Attendee.java
@@ -0,0 +1,9 @@
+package com.example.eventscan.Entities;
+
+import com.example.eventscan.User;
+
+public class Attendee extends User {
+    public Attendee(String name, String password) {
+        super(name, password);
+    }
+}
