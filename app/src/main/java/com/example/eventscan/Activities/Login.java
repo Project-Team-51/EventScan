d727076b37f5d5fd4b32d805dd1ba3b7faa53eee
Bern Kairo Aran <bern@ualberta.ca>
2024-03-01 21:22:29 -0700

    added firebase stuff
    added firebase stuff, admin button now takes you to the login page

--- app/src/main/java/com/example/eventscan/Activities/Login.java
+++ app/src/main/java/com/example/eventscan/Activities/Login.java
@@ -1,27 +1,41 @@
 package com.example.eventscan.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.eventscan.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.login_activity);
 
-        EditText editTextUsername = findViewById(R.id.editTextUsername);
-        EditText editTextPassword = findViewById(R.id.editTextPassword);
+
+        EditText email = findViewById(R.id.editTextUsername);
+        EditText password = findViewById(R.id.editTextPassword);
         Button buttonLogin = findViewById(R.id.buttonLogin);
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                String username = editTextUsername.getText().toString();
-                String password = editTextPassword.getText().toString();
+                String user = email.getText().toString();
+                String pass = password.getText().toString();
+
 
             }
         });
--- app/src/main/java/com/example/eventscan/Activities/UserSelection.java
+++ app/src/main/java/com/example/eventscan/Activities/UserSelection.java
@@ -3,8 +3,8 @@ package com.example.eventscan.Activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.eventscan.Entities.Administrator;
@@ -12,18 +12,28 @@ import com.example.eventscan.Entities.Attendee;
 import com.example.eventscan.Entities.Organizer;
 import com.example.eventscan.Entities.User;
 import com.example.eventscan.R;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserSelection extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.userselection);
 
         // Handle user type selection
         Button userType1Button = findViewById(R.id.buttonOrganizer);
         Button userType2Button = findViewById(R.id.buttonAttendee);
-        Button userType3Button = findViewById(R.id.buttonAdministrator);
+        final Button adminButton = findViewById(R.id.buttonAdministrator);
 
+        adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserSelection.this, Login.class);
+                startActivity(intent);
+            }
+        });
         //userType1Button.setOnClickListener(view -> startApp(Organizer.class));
         //userType2Button.setOnClickListener(view -> startApp(Attendee.class));
         //userType3Button.setOnClickListener(view -> startApp(Administrator.class));
