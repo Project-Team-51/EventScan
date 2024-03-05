ac0422cf7ba023c1245ed9eed21cce94601223b1
Talal Khan <t.khan040604@gmail.com>
2024-03-04 18:09:10 -0700

    Add organizer homepage activity

---
+++ app/src/main/java/com/example/eventscan/Activities/OrganizerHomePage.java
@@ -0,0 +1,21 @@
+package com.example.eventscan.Activities;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.eventscan.R;
+import com.google.firebase.FirebaseApp;
+
+public class OrganizerHomePage extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.organizer_homepage);
+
+    }
+}
