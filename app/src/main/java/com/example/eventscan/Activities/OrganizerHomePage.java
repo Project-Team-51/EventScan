eb4ce5042e05d5edad68967ef260ffb12040a093
Talal Khan <t.khan040604@gmail.com>
2024-03-04 21:34:15 -0700

    Update

--- app/src/main/java/com/example/eventscan/Activities/OrganizerHomePage.java
+++ app/src/main/java/com/example/eventscan/Activities/OrganizerHomePage.java
@@ -1,14 +1,10 @@
 package com.example.eventscan.Activities;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.eventscan.R;
-import com.google.firebase.FirebaseApp;
 
 public class OrganizerHomePage extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/eventscan/Activities/OrganizerProfile.java
@@ -0,0 +1,4 @@
+package com.example.eventscan.Activities;
+
+public class OrganizerProfile {
+}
