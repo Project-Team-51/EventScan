a1564e84594ff47a9bcc426b6a10c358649fd034
“Abdullah <abdullah.faisal.karim@gmail.com>
2024-03-06 10:26:35 -0700

    Initializing Attendee Profile Fragment

--- app/src/main/java/com/example/eventscan/Entities/User.java
+++ app/src/main/java/com/example/eventscan/Entities/User.java
@@ -25,6 +25,8 @@ public class User {
     // empty constructor so it works with firestore
     public User() {}
 
+    }
+
     public User(String name, String deviceID, String email, String phoneNum, String bio, String profilePictureID) {
             this.name = name;
             this.deviceID = deviceID;
