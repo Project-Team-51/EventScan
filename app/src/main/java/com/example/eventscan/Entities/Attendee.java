2e5417ae60eed2a647a104b62b6d834c6de3f5d4
“Abdullah <abdullah.faisal.karim@gmail.com>
2024-03-05 22:22:38 -0700

    Attendee parameters

--- app/src/main/java/com/example/eventscan/Entities/Attendee.java
+++ app/src/main/java/com/example/eventscan/Entities/Attendee.java
@@ -1,7 +1,7 @@
 package com.example.eventscan.Entities;
 
 public class Attendee extends User {
-    public Attendee(String name, String password) {
-        super(name, password);
+    public Attendee(String name, String deviceID) {
+        super(name, deviceID);
     }
 }
