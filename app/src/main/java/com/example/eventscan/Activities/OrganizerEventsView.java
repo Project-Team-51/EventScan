1e055c39b2b3a18fd6df71b02503b6db05abd0a6
dottemeod <151109137+dottemeod@users.noreply.github.com>
2024-03-06 17:43:12 -0700

    Organizer button for ViewEvent functionality

--- app/src/main/java/com/example/eventscan/Activities/OrganizerEventsView.java
+++ app/src/main/java/com/example/eventscan/Activities/OrganizerEventsView.java
@@ -14,9 +14,21 @@ public class OrganizerEventsView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.organizer_events_view);
+
         final Button buttonAddEvent = findViewById(R.id.buttonAddEvent);
         final Button buttonOrganizerProfile = findViewById(R.id.buttonOrganizerProfile);
         final Button buttonSendNoti = findViewById(R.id.buttonSendEventNoti);
+        final Button buttonViewEvents = findViewById(R.id.buttonViewEvents);
+
+        buttonViewEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Set the content view to organizer_view_all.xml
+                Intent intent = new Intent(OrganizerEventsView.this, OrganizerViewAllEvents.class);
+                startActivity(intent);
+            }
+        });
+
 
         buttonOrganizerProfile.setOnClickListener(new View.OnClickListener() {
             @Override
