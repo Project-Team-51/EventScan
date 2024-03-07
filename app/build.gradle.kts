e515762bd5bab066ec7864fd57d4490919e58c65
Alec <50636059+alec-zaiane@users.noreply.github.com>
2024-03-07 15:52:26 -0700

    QR Scan page now has a preview of the camera

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,12 +39,10 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-
-
-
-
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("com.google.mlkit:barcode-scanning:17.2.0")
 
     implementation ("com.github.dhaval2404:imagepicker:2.1")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
@@ -55,4 +53,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    val camerax_version = "1.4.0-alpha04"
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation("androidx.camera:camera-view:${camerax_version}")
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
 }
---
+++ app/src/main/java/com/example/eventscan/Fragments/QrScannerFragment.java
@@ -0,0 +1,69 @@
+package com.example.eventscan.Fragments;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
+
+
+import com.example.eventscan.R;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
+public class QrScannerFragment extends Fragment {
+    //https://developer.android.com/media/camera/camerax/preview
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private PreviewView previewView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this.requireContext());
+        View view = getLayoutInflater().inflate(R.layout.scan_qr_layout, container, false);
+        previewView = view.findViewById(R.id.CameraPreview);
+        checkCameraPermissions(this.requireContext());
+        try {
+            bindPreview(cameraProviderFuture.get());
+            Log.i("Camera View", "should be bound");
+        } catch (ExecutionException | InterruptedException e) {
+            Log.e("Camera View", "Not working :(");
+            throw new RuntimeException(e);
+        }
+        return view;
+    }
+
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder().build();
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
+    }
+
+    private static void checkCameraPermissions(Context context){
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // need to ask for permission
+            ActivityCompat.requestPermissions((Activity) context, new String[] {Manifest.permission.CAMERA }, 100);
+        }
+    }
+}
