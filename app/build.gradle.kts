adb25db3ba2c053b39f7dd3f027723449149ee52
dottemeod <151109137+dottemeod@users.noreply.github.com>
2024-03-08 06:53:33 -0700

    added to manifest and gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,10 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+
+
+    implementation ("com.journeyapps:zxing-android-embedded:4.2.0")
+
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.recyclerview:recyclerview:1.3.0")
