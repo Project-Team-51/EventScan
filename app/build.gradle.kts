714ca172364d07dc90509b35c1428f218af219a1
sobaia <sobaia@ualberta.ca>
2024-03-08 15:02:11 -0700

    UserSelectionEspressoTest complete
    added espresso test for user selection buttons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,12 @@ dependencies {
     implementation("androidx.camera:camera-camera2:${camerax_version}")
     implementation("androidx.camera:camera-view:${camerax_version}")
     implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation ("androidx.test:rules:1.4.0")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation ("androidx.test.espresso:espresso-intents:3.4.0")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation ("androidx.fragment:fragment-testing:1.4.0")
 }
