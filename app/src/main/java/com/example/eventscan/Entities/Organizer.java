a80702553e92c345e70d25aa15468b1b4f8dfd0c
Alec <50636059+alec-zaiane@users.noreply.github.com>
2024-03-11 12:51:17 -0600

    no errors I'm surprised

--- app/src/main/java/com/example/eventscan/Entities/Organizer.java
+++ app/src/main/java/com/example/eventscan/Entities/Organizer.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 /*
  * Organizer subclass of the user class. As is, doesnt have any extra features.
  */
-public class Organizer extends User {
+public class Organizer extends Attendee {
     //public ArrayList<Event> ownedEvents;
     // empty constructor so it works with firestore
 
