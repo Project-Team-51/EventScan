a207a8574217c35ce5f4ba74417b792a1daf849a
Alec <50636059+alec-zaiane@users.noreply.github.com>
2024-04-04 00:18:49 -0600

    small change so I can push again

--- app/src/main/java/com/example/eventscan/Fragments/EventFragment.java
+++ app/src/main/java/com/example/eventscan/Fragments/EventFragment.java
@@ -111,6 +111,7 @@ public class EventFragment extends Fragment implements DeleteEvent.DeleteEventLi
             }
         });
 
+
         ownedEventsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
