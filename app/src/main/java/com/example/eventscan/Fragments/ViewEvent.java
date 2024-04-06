43cdbfd74618555ed097b7a1057ad9387735d2e2
“Abdullah <abdullah.faisal.karim@gmail.com>
2024-04-06 14:14:11 -0600

    merging main branch 7

--- app/src/main/java/com/example/eventscan/Fragments/ViewEvent.java
+++ app/src/main/java/com/example/eventscan/Fragments/ViewEvent.java
@@ -20,6 +20,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+
 import com.bumptech.glide.Glide;
 
 import com.example.eventscan.Database.Database;
