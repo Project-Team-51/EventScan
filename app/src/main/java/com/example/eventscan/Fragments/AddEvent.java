fb8dd1112efdcb0c5221b6cb26f5c7b8df167168
sobaia <sobaia@ualberta.ca>
2024-04-05 20:16:06 -0600

    deleted poster upload

--- app/src/main/java/com/example/eventscan/Fragments/AddEvent.java
+++ app/src/main/java/com/example/eventscan/Fragments/AddEvent.java
@@ -36,6 +36,8 @@ import com.example.eventscan.Entities.Organizer;
 import com.example.eventscan.R;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -320,6 +322,9 @@ public class AddEvent extends DialogFragment {
                         posterUri = result;
                         imageView.setImageURI(result);
                         posterUriString = posterUri.toString();
+                        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+                        StorageReference profilePicRef = storageRef.child("poster_pics").child(deviceID);
+                        profilePicRef.putFile(posterUri);
                     }
 
                 }
