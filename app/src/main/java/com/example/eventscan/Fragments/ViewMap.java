3a070e49f2e9d7642e58f3b66455a9e3aace056f
sobaia <sobaia@ualberta.ca>
2024-04-06 22:31:05 -0600

    Fine tuned map
    Set map zoom, center, markers.

--- app/src/main/java/com/example/eventscan/Fragments/ViewMap.java
+++ app/src/main/java/com/example/eventscan/Fragments/ViewMap.java
@@ -7,54 +7,36 @@ import android.app.Dialog;
 
 
 import android.os.Bundle;
-import android.provider.Settings;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
 
-
-import com.bumptech.glide.Glide;
 
 import com.example.eventscan.Database.Database;
 
-import com.bumptech.glide.request.RequestOptions;
-
-import com.example.eventscan.Database.Database;
-import com.example.eventscan.Database.DatabaseHelper;
-
-
-import com.example.eventscan.Entities.Attendee;
-import com.example.eventscan.Entities.DeviceID;
 import com.example.eventscan.Entities.Event;
 import com.example.eventscan.R;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
-import org.osmdroid.api.IGeoPoint;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Objects;
+
 
 
 public class ViewMap extends DialogFragment {
 
     Database db;
-    ArrayList<IGeoPoint> points;
+    ArrayList<GeoPoint> points = new ArrayList<>(); // Initialize the ArrayList
+
     /**
      * Default constructor for the ViewEvent DialogFragment.
      */
@@ -67,6 +49,7 @@ public class ViewMap extends DialogFragment {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         assert getArguments() != null;
         LayoutInflater inflater = requireActivity().getLayoutInflater();
+        Event selectedEvent = (Event) getArguments().getSerializable("selectedEvent");
         View view = inflater.inflate(R.layout.map_view, null);
 
         Dialog dialog = new Dialog(requireContext());
@@ -78,10 +61,17 @@ public class ViewMap extends DialogFragment {
 
         FirebaseStorage storage = FirebaseStorage.getInstance();
         db = Database.getInstance();
+        db.geolocation.getEventCheckinPoints(selectedEvent);
 
         mapView.setTileSource(TileSourceFactory.MAPNIK); // Use the default OpenStreetMap tile source
         mapView.setMultiTouchControls(true); // Enable multi-touch controls
 
+        // Add some GeoPoints to the ArrayList (example)
+        points.add(new GeoPoint(53.468922, -113.599228));
+        points.add(new GeoPoint(53.405300, -113.574860));
+
+        GeoPoint house = new GeoPoint(53.405300, -113.574860);
+        // Iterate over the ArrayList and add markers to the map
         for (GeoPoint geoPoint : points) {
             Marker marker = new Marker(mapView);
             marker.setPosition(geoPoint);
@@ -89,6 +79,8 @@ public class ViewMap extends DialogFragment {
             mapView.getOverlays().add(marker);
         }
 
+        mapView.getController().setZoom(12.0);
+        mapView.getController().setCenter(house);
         returnView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
