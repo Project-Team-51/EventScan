0e532530ad7c063161ea37d9e807bb2296ba06b0
sobaia <sobaia@ualberta.ca>
2024-04-08 17:27:13 -0600

    java docs

---
+++ doc/javadocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.eventscan"},{"l":"com.example.eventscan.Activities"},{"l":"com.example.eventscan.Database"},{"l":"com.example.eventscan.Entities"},{"l":"com.example.eventscan.Fragments"},{"l":"com.example.eventscan.Helpers"}];updateSearchResults();
